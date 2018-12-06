fafad76ab3545de54592a12ac71186058f7f6b0e
Juan Sebastián Jaramillo <juansesin@gmail.com>
2018-12-06 15:58:15 -0500

    Create secure.sql

---
+++ secure.sql
@@ -0,0 +1,10 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+UPDATE mysql.user SET authentication_string=PASSWORD('${db_root_password}') WHERE User='root';
+DELETE FROM mysql.user WHERE User='';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+create database sitioweb;
+CREATE USER 'us_sitioweb'@'localhost' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON sitioweb.* TO 'us_sitioweb'@'localhost';
