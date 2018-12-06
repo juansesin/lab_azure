baa160af299de6d4cdaa8382333b2774d23b904e
Juan Sebastián Jaramillo <juansesin@gmail.com>
2018-12-06 16:09:57 -0500

    Create configurar.sh

---
+++ configurar.sh
@@ -0,0 +1,3 @@
+apt install -y mysql-server mysql-client apache2 apache2-data apache2-utils php libapache2-mod-php php-mysql php-curl php-gd php-cli php-json php-xml php-zip
+mysql < secure.sql
+tar xvfz website.tgz /var
