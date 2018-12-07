c3830cf0a120c98e81c9b6d9f73d31277374d4b9
Juan Sebastián Jaramillo <juansesin@gmail.com>
2018-12-07 11:39:46 -0500

    Update configurar.sh

--- configurar.sh
+++ configurar.sh
@@ -1,4 +1,4 @@
-apt install -y mysql-server mysql-client apache2 apache2-data apache2-utils php libapache2-mod-php php-mysql php-curl php-gd php-cli php-json php-xml php-zip php-pgsql postgresql-client-common
+apt install -y mysql-server mysql-client apache2 apache2-data apache2-utils php libapache2-mod-php php-mysql php-curl php-gd php-cli php-json php-xml php-zip php-pgsql postgresql-client-common postgresql-client-10
 mysql < secure.sql
 tar xvfz sitioweb.tgz -C /var/www/html
 mysql -u us_sitioweb -ppassword sitioweb < sitioweb.sql
